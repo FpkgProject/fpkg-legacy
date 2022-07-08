448165174adc325ed15602228cd32c9040de5499
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-07-07 21:14:28 -0300

    update: add function to verify if package is found or not

--- src/main.go
+++ src/main.go
@@ -6,13 +6,16 @@ import (
 
 var PACKAGENOTFOUND = "The package not found in list of repo"
 var PACKAGEFOUND = "The package are found in list of repo"
+var LOCKED = false
+
+func is_locked(Locked bool) {
+	if Locked == true {
+		fmt.Printf("%s\n", PACKAGEFOUND)
+	} else {
+		fmt.Printf("%s\n", PACKAGENOTFOUND)
+	}
+}
 
 func main() {
-
-	var locked = true
-	if locked == false {
-		fmt.Printf("%s\n", PACKAGENOTFOUND)
-	} else {
-		fmt.Printf("%s\n", PACKAGEFOUND)
-	}
+	is_locked(LOCKED)
 }
