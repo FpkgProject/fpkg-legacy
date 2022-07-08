4226e763107f71dc0a31c7e52acce283f11029c4
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-07-08 14:06:36 -0300

    send the clearfile to rebuild a main function

--- src/main.go
+++ src/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-var PACKAGENOTFOUND = "The package not found in list of repo"
-var PACKAGEFOUND = "The package are found in list of repo"
-var LOCKED = false
-
-func is_locked(Locked bool) {
-	if Locked == true {
-		fmt.Printf("%s\n", PACKAGEFOUND)
-	} else {
-		fmt.Printf("%s\n", PACKAGENOTFOUND)
-	}
-}
+import ()
 
 func main() {
-	is_locked(LOCKED)
+
 }
