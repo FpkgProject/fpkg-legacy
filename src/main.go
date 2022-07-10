0640f5ac614af68763666acbf57777ccbb142466
Redson <redson@riseup.net>
2022-07-10 12:01:27 -0300

    fix: typos

--- src/main.go
+++ src/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 var (
-	MAKEFILENOTFOUND  string = "The Makefile not is found in this repository"
-	CONFIGURENOTFOUND string = "The ./configure not is found in this repository"
+	MAKEFILENOTFOUND  string = "MakeFile not found in this repository"
+	CONFIGURENOTFOUND string = "./configure not found in this repository"
 	MAKEFILEexists    bool   = false
 	CONFIGUREexists   bool   = false
 )
@@ -28,37 +28,37 @@ func removeTmpDirectory() {
 	os.Remove("tmp")
 }
 
-// ----------------------------------------------- List of Function in Fpkg ---------------------------------
+// ----------------------------------------------- List of fpkg functions -----------------------------------
 
 func addRepo(RepoName string) {
 	if _, err := os.Stat("./repoList.txt"); errors.Is(err, os.ErrNotExist) {
 		createRepoList()
-		fmt.Printf("adding a repo %s\n", RepoName)
+		fmt.Printf("adding the repo %s\n", RepoName)
 	} else {
-		fmt.Printf("adding a repo %s\n", RepoName)
+		fmt.Printf("adding the repo %s\n", RepoName)
 	}
 }
 
 func delRepo(RepoName string) {
-	fmt.Printf("Removing a repo %s\n", RepoName)
+	fmt.Printf("Removing the repo %s\n", RepoName)
 }
 
 func installPackage(PackageName string) {
 	createTmpDirectory()
-	fmt.Printf("Installing a %s\n", PackageName)
+	fmt.Printf("Installing %s\n", PackageName)
 	//defer removeTmpDirectory()
 }
 
 func uninstallPackage(PackageName string) {
-	fmt.Printf("uninstalling a %s\n", PackageName)
+	fmt.Printf("Removing %s\n", PackageName)
 }
 
 // -------------------------------------------------- CLI ---------------------------------------------------
 func cli() {
-	installFlag := flag.String("install", "", "install a new package")
-	uninstallFlag := flag.String("uninstall", "", "uninstall a package from system")
-	addrepo := flag.String("addrepo", "", "adding a new repo in repoList")
-	delrepo := flag.String("delrepo", "", "removing a repo in repoList")
+	installFlag := flag.String("install", "", "Install a new package")
+	uninstallFlag := flag.String("uninstall", "", "Remove a package from the system")
+	addrepo := flag.String("addrepo", "", "Add a new repo to the repoList")
+	delrepo := flag.String("delrepo", "", "Remove a repo to the repoList")
 
 	flag.Parse()
 
