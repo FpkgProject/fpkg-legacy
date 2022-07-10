bada3325cbd92b4dde509999cdc566caaf3d0742
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-07-10 09:37:21 -0300

    adding new fake functions to Fpkg

--- src/main.go
+++ src/main.go
@@ -5,13 +5,45 @@ import (
 	"fmt"
 )
 
-func main() {
+// ----------------------------------------------- List of Function in Fpkg ---------------------------------
+
+func addRepo(RepoName string) {
+	fmt.Printf("adding a repo %s\n", RepoName)
+}
+
+func delRepo(RepoName string) {
+	fmt.Printf("Removing a repo %s\n", RepoName)
+}
+
+func installPackage(PackageName string) {
+	fmt.Printf("Installing a %s\n", PackageName)
+}
+
+func uninstallPackage(PackageName string) {
+	fmt.Printf("uninstalling a %s\n", PackageName)
+}
+
+// -------------------------------------------------- CLI ---------------------------------------------------
+func cli() {
 	installFlag := flag.String("install", "", "install a new package")
-	newrepo := flag.String("addrepo", "", "adding a new repo in repoList")
+	uninstallFlag := flag.String("uninstall", "", "uninstall a package from system")
+	addrepo := flag.String("addrepo", "", "adding a new repo in repoList")
+	delrepo := flag.String("delrepo", "", "removing a repo in repoList")
+
 	flag.Parse()
+
 	if *installFlag != "" {
-		fmt.Println(*installFlag)
-	} else if *newrepo != "" {
-		fmt.Println(*newrepo)
+		installPackage(*installFlag)
+	} else if *addrepo != "" {
+		addRepo(*addrepo)
+	} else if *uninstallFlag != "" {
+		uninstallPackage(*uninstallFlag)
+	} else if *delrepo != "" {
+		delRepo(*delrepo)
 	}
+}
+
+// -------------------------------------------------- Main Function ----------------------------------------
+func main() {
+	cli()
 }
