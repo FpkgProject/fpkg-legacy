32e23fd26f82af6cc88f050c13209a25b9c4b496
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-07-11 10:22:22 -0300

    fix: remove unused error variables

--- src/main.go
+++ src/main.go
@@ -1,12 +1,5 @@
 package main
 
-var (
-	MAKEFILENOTFOUND  string = "MakeFile not found in this repository"
-	CONFIGURENOTFOUND string = "./configure not found in this repository"
-	MAKEFILEexists    bool   = false
-	CONFIGUREexists   bool   = false
-)
-
 // -------------------------------------------------- Main Function ----------------------------------------
 func main() {
 	Cli()
