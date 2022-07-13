8fd0451ca36b02f4be3795b4d8f4d17cbde78b89
Redson <redson@riseup.net>
2022-07-13 17:34:24 -0300

    chore(colors var): More human readable

--- src/funcs/colors.go
+++ src/funcs/colors.go
@@ -2,27 +2,27 @@ package funcs
 
 
 type colors struct {
-    Reset string
-    Black string
-    Red string
-    Green string
-    Yellow string
-    Blue string
-    Purple string
-    Cyan string
-    White string
+  Reset string
+  Black string
+  Red string
+  Green string
+  Yellow string
+  Blue string
+  Purple string
+  Cyan string
+  White string
 }
 
 
 var Color = colors {
-    "\033[m",
-    "\033[30m",
-    "\033[31m",
-    "\033[32m",
-    "\033[33m",
-    "\033[34m",
-    "\033[35m",
-    "\033[36m",
-    "\033[37m",
+  Reset:    "\033[m",
+  Black:  "\033[30m",
+  Red:    "\033[31m",
+  Green:  "\033[32m",
+  Yellow: "\033[33m",
+  Blue":   \033[34m",
+  Purple: "\033[35m",
+  Cyan:   "\033[36m",
+  White:  "\033[37m",
 }
 
