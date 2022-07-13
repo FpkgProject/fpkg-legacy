f2207b5310eaf7adac29db3f297fe5291b6df117
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-07-13 11:54:17 -0300

    remake a main.go

--- src/main.go
+++ src/main.go
@@ -8,39 +8,34 @@ import (
 	"text/tabwriter"
 )
 
+func searchPackage(RepositorySearch string) {
+	fmt.Printf(":: Search for a package: ")
+	repos, err := funcs.SearchRepos(RepositorySearch)
+	if err != nil {
+		log.Fatal("Cannot get the repositories list, please check network connection.")
+	}
 
-func main(){
-  var (
-    reposcan string
-  )
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
 
-  fmt.Printf(":: Search for a package: ")
-  fmt.Scanln(&reposcan)
+	defer w.Flush()
 
-  repos, err := funcs.SearchRepos(reposcan)
-  if err != nil {
-    log.Fatal("Cannot get the repositories list, please check network connection.")
-  }
+	fmt.Fprintf(w, "\n %s\t%s\t%s\t", "Repository", "Stars", "IsFork")
+	fmt.Fprintf(w, "\n %s\t%s\t%s\t", "----------", "-----", "------")
 
-  w := new(tabwriter.Writer)
-  w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+	if len(repos.Repositories) == 0 {
+		log.Fatal("Package not found.")
+	}
+	if len(repos.Repositories) < SizeOfList {
+		SizeOfList = len(repos.Repositories)
+	}
 
-  defer w.Flush()
-
-  fmt.Fprintf(w, "\n %s\t%s\t%s\t", "Repository", "Stars", "IsFork")
-  fmt.Fprintf(w, "\n %s\t%s\t%s\t", "----------", "-----", "------")
-  
-  if len(repos.Repositories) == 0 {
-    log.Fatal("Package not found.")
-  }
-  if len(repos.Repositories) < SizeOfList {
-    SizeOfList = len(repos.Repositories)
-  }
-
-  for i := 0; i < SizeOfList;i++ {
-    fmt.Fprintf(w, "\n [%d] %s\t%d\t%s\t", i,
-    repos.Repositories[i].GetFullName(),
-    repos.Repositories[i].GetStargazersCount(),
-    funcs.IsFork(repos.Repositories[i].GetFork()))
-  }
+	for i := 0; i < SizeOfList; i++ {
+		fmt.Fprintf(w, "\n [%d] %s\t%d\t%s\t", i,
+			repos.Repositories[i].GetFullName(),
+			repos.Repositories[i].GetStargazersCount(),
+			funcs.IsFork(repos.Repositories[i].GetFork()))
+	}
 }
+
+func main() {}
