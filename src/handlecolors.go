6bcbbf72e3f57df06a360f3250c67763d8aad127
Redson <redson@riseup.net>
2022-07-13 20:58:28 -0300

    chore: rename colors.go

--- src/handlecolors.go
+++ src/handlecolors.go
@@ -1,28 +1,37 @@
-package funcs
-
+package main
 
 type colors struct {
   Reset string
-  Black string
+  //Black string
   Red string
   Green string
   Yellow string
   Blue string
-  Purple string
+  //Purple string
   Cyan string
   White string
 }
 
-
-var Color = colors {
+var colorArray = []colors {
+  {
   Reset:    "\033[m",
-  Black:  "\033[30m",
+  //Black:  "\033[30m",
   Red:    "\033[31m",
   Green:  "\033[32m",
   Yellow: "\033[33m",
   Blue:   "\033[34m",
-  Purple: "\033[35m",
+  //Purple: "\033[35m",
   Cyan:   "\033[36m",
   White:  "\033[37m",
+  },
+  { "", "", "", "", "", "", "" },
+}
+
+var Color = colorArray[1]
+
+func init(){
+  if ColorBool {
+    Color = colorArray[0]
+  }
 }
 
