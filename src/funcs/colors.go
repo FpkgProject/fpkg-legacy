f9223ff70a461b2759619e49e5cd68ecf9beac03
Redson <redson@riseup.net>
2022-07-13 17:37:58 -0300

    fix(colors): typo

--- src/funcs/colors.go
+++ src/funcs/colors.go
@@ -20,7 +20,7 @@ var Color = colors {
   Red:    "\033[31m",
   Green:  "\033[32m",
   Yellow: "\033[33m",
-  Blue":   \033[34m",
+  Blue:   "\033[34m",
   Purple: "\033[35m",
   Cyan:   "\033[36m",
   White:  "\033[37m",
