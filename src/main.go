6995e3fa7df3984e18ff98cd017dcdff7aa92428
Redson <redson@riseup.net>
2022-07-13 07:33:03 -0300

    feat: First interface.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,50 @@
 package main
 
-// -------------------------------------------------- Main Function ----------------------------------------
-func main() {
-	Cli()
+import (
+	"fmt"
+	"fpkg/funcs"
+	"log"
+	"os"
+	"text/tabwriter"
+)
+
+
+func main(){
+  var (
+    showlist int = 5
+    reposcan string
+    
+  )
+
+  fmt.Printf(":: Search for a package: ")
+  fmt.Scanln(&reposcan)
+
+  repos, err := funcs.SearchRepos(reposcan)
+  if err != nil {
+    log.Fatal("Cannot get the repositories list, please check network connection.")
+  }
+
+  w := new(tabwriter.Writer)
+  w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+
+  defer w.Flush()
+
+  fmt.Fprintf(w, "\n %s\t%s\t%s\t", "Repository", "Stars", "IsFork")
+  fmt.Fprintf(w, "\n %s\t%s\t%s\t", "----------", "-----", "------")
+  
+  if len(repos.Repositories) == 0 {
+    log.Fatal("Package not found.")
+  }
+  if len(repos.Repositories) < showlist {
+    showlist = len(repos.Repositories)
+  }
+
+  for i := 0; i < showlist;i++ {
+    fmt.Fprintf(w, "\n [%d] %s\t%d\t%s\t", i,
+    repos.Repositories[i].GetFullName(),
+    repos.Repositories[i].GetStargazersCount(),
+    funcs.IsFork(repos.Repositories[i].GetFork()))
+  }
+
+  Cli()
 }
