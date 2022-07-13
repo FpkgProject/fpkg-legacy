a38f88994a162f63bec483513093578b39bac244
Redson <redson@riseup.net>
2022-07-13 20:59:21 -0300

    feat(flags): No flags handler

--- src/flags.go
+++ src/flags.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "fmt"
+  "os"
   "github.com/spf13/pflag"
 )
 
@@ -10,6 +12,12 @@ func init() {
   pflag.StringVarP(&InstallFlag, "install", "i", "", "Install a new package.")
   pflag.StringP("remove", "r", "", "Remove a package.")
   pflag.Parse()
+
+  if len(os.Args) < 2 {
+    fmt.Println("\033[1;31m" + "error:" + Color.Reset, "no operation specified.\noperations:")
+    pflag.PrintDefaults()
+    os.Exit(1)
+  }
 
   //UninstallFlag := flag.String("uninstall", "", "Remove a package from the system")
 
