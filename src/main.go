6dd4aeb31631f2980cca71b283c42e7f61324125
Redson <redson@riseup.net>
2022-07-13 21:02:06 -0300

    chore: remove extra linebreak

--- src/main.go
+++ src/main.go
@@ -8,7 +8,6 @@ import (
 
 
 func main(){
-
   repos, err := funcs.SearchRepos(InstallFlag)
   if err != nil {
     log.Fatal("Cannot get the repositories list, please check network connection.")
@@ -23,34 +22,17 @@ func main(){
   }
 
 
-  if true {
-    for i := 0; i < SizeOfList;i++ {
-      fmt.Printf("\n\n%s[%d]\033[m %s->\033[m %s%s\033[m",
-      funcs.Color.Blue, i, funcs.Color.Green,
-      funcs.Color.Cyan, repos.Repositories[i].GetFullName())
+  for i := 0; i < SizeOfList;i++ {
+    fmt.Printf("\n%s[%d]\033[m %s->\033[m %s%s\033[m",
+    Color.Blue, i, Color.Green,
+    Color.Cyan, repos.Repositories[i].GetFullName())
 
-      fmt.Printf("\n\t%s",
-      funcs.GetDescriptionText(repos.Repositories[i].GetDescription()))
+    fmt.Printf("\n\t%s",
+    funcs.GetDescriptionText(repos.Repositories[i].GetDescription()))
 
-      fmt.Printf("\n\t%sStars: %d %s\033[m",
-      funcs.Color.Yellow,
-      repos.Repositories[i].GetStargazersCount(),
-      funcs.GetForkText(repos.Repositories[i].GetFork()))
-    }
-
+    fmt.Printf("\n\t%sStars: %d %s\033[m",
+    Color.Yellow,
+    repos.Repositories[i].GetStargazersCount(),
+    funcs.GetForkText(repos.Repositories[i].GetFork()))
   }
-/*
-    for i := 0; i < SizeOfList;i++ {
-      fmt.Printf("\n\n[%d] -> %s",
-        i, repos.Repositories[i].GetFullName())
-
-      fmt.Printf("\n\t%s",
-        funcs.GetDescriptionText(repos.Repositories[i].GetDescription()))
-
-      fmt.Printf("\n\tStars: %d %s",
-        repos.Repositories[i].GetStargazersCount(),
-        funcs.GetForkText(repos.Repositories[i].GetFork()))
-    }
-  }
-  */
 }
