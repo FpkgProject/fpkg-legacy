2da0e5917d20c59392e64a586778d002b8f57b02
Redson <redson@riseup.net>
2022-07-14 08:58:32 -0300

    feat: Get Clone URL.

--- src/funcs/gh-api.go
+++ src/funcs/gh-api.go
@@ -1,18 +1,22 @@
 package funcs
 
-import(
+import (
   "github.com/google/go-github/github"
   "context"
 )
 
 var client = github.NewClient(nil)
 
-func SearchRepos(reposearch string) (*github.RepositoriesSearchResult, error) {
-  repos, _, err := client.Search.Repositories(context.Background(), reposearch, nil)
-
-  return repos, err
+func GetRepoURL(owner string, name string) (string, error) {
+  req, _, err := client.Repositories.Get(context.Background(), owner, name)
+  repo := req.GetCloneURL()
+  return repo, err
 }
 
+func SearchRepos(reposearch string) (*github.RepositoriesSearchResult, error) {
+  repos, _, err := client.Search.Repositories(context.Background(), reposearch, nil)
+  return repos, err
+}
 
 func GetForkText(fork bool) string {
   if fork {
